6956f4d3aa3aa878609a3102f3c775971b701fad
Frehitzz <fritzharlydegamo@gmail.com>
2025-05-27 00:41:20 +0800

    successfully created and give each their perspective functions

---
+++ 07 - CALCULATOR/script.js
@@ -0,0 +1,47 @@
+const display = document.getElementById("display");
+
+display.value = "0";
+
+function clearAll(){
+    display.value = "0";
+}
+
+function deleteLast() {
+   let current = display.value;
+    // Only slice if not already "0"
+    if (current !== "0") {
+        current = current.slice(0, -1);
+        display.value = current === "" ? "0" : current;
+    }
+}
+
+//to display the selected button
+function todisplay(input){
+    if(input === "%"){
+        calculatePercentage();
+        return;
+    }
+
+    if (display.value === "0" && input !== ".") {
+        display.value = input;
+    } else {
+        display.value += input;
+    }
+
+}
+
+function calculatePercentage() {
+    try {
+        display.value = eval(display.value) / 100;
+    } catch (error) {
+        display.value = "Error";
+    }
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    } catch (error){
+        display.value = "Error";
+    }
+}
